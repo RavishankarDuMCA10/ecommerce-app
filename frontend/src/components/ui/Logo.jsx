1aac4183052ddcba30aa76c7675caf9172c61fc7
Ravi Shankar Kushwaha <ravis.du.mca10@gmail.com>
2026-04-29 17:48:21 +0530

    Change logo text from 'Ecommerce' to 'Ecomm'

--- frontend/src/components/ui/Logo.jsx
+++ frontend/src/components/ui/Logo.jsx
@@ -9,7 +9,7 @@ const Logo  = () => {
             <span className="text-black text-3xl">
                 <FiShoppingBag />
             </span>                
-            <span className="ml-3 text-xl">Ecommerce</span>
+            <span className="ml-3 text-xl">Ecomm</span>
         </Link>
     </>
   )
